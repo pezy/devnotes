0bbcb8162d66c11968c4ef1c103cabd044941114
pezy <urbancpz@gmail.com>
2017-05-02 13:36:19 +0800

    init with old content

---
+++ docs/notes/using_xmanager_to_connect_to_remote_centos7_via_xdmcp.md
@@ -0,0 +1,48 @@
+Title: Using Xmanager to connect to remote CentOS 7 via XDMCP
+Date: 2016-06-08
+Category: devops
+
+- Enable EPEL Repository:
+
+```shell
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
+rpm -ivh epel-release-7-6.noarch.rpm
+```
+
+- install `lightdm`, `xfce` and configure it:
+
+```shell
+su -
+yum install lightdm && yum groupinstall xfce
+vi /etc/lightdm/lightdm.conf
+​```
+[XDMCPServer]
+enabled=true
+user-session=xfce
+​```
+```
+
+- change the default display manager to `lightdm`
+
+```shell
+systemctl disable gdm && systemctl enable lightdm
+systemctl stop gdm && systemctl start lightdm
+systemctl stop firewalld.service
+```
+
+----
+
+- QtCreator open failed, reporting: ` Assertion !xcb_xlib_too_much_data_requested' failed.`
+
+```shell
+vi ~/.local/share/applications/DigiaQt-qtcreator-community.desktop
+​```
+Exec=xxx - noload Welcome
+​```
+```
+
+-----
+
+- Qt: XKEYBOARD extension not present on the X server.
+
+ ![xmanager_XDMCP_config](..\img\xmanager_XDMCP_config.png)
---
+++ pelicanconf.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'pezy'
+SITENAME = 'devnotes'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['notes', 'img', 'lab', 'res']
+ARTICLE_PATHS = ['notes']
+ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{slug}.html'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'Chinese (Simplified)'
+THEME = 'chameleon'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+#Template
+BS3_URL = '//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css'
+BS3_JS  = '//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js'
+BS3_THEME = '//cdn.bootcss.com/bootswatch/3.3.7/cosmo/bootstrap.min.css'
+
+MENUITEMS = [
+    ('Home', '/'),
+    ('Archives', [
+        ('Tags', '/tags.html'),
+        ('Categories', '/categories.html'),
+        ('Chronological', '/archives.html'),
+        ]),
+    ('Links', [
+        ('old Blog', 'http://blog.devnotes.org'),
+        ('diary', 'http://devnotes.org/diary'),
+        ('reading', '../lab/reading.html'),
+        ('list', '../lab/list.html'),
+        ]),
+    ('Social', [
+        ('Email', 'mailto: pezy.dev@gmail.com'),
+        ('Github', 'http://github.com/pezy'),
+        ('Zhihu', 'https://www.zhihu.com/people/pezy'),
+        ]),
+    ]
+
+FAVICON = u'/res/favicon.png'
+FAVICON_TYPE = u'png'
+
+AUTHORS = {
+    u'pezy': '/about.html',
+}
+
+DISQUS_SITENAME = 'devnotes-org'
+GITHUB_URL = 'https://github.com/pezy/pezy.github.io'
